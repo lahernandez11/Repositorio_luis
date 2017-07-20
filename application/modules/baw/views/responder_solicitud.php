12ef4db68b45126c825be740870b6120f6fc941d
LAHernandezg <LAHernandezg@LERDES3-LAP>
2017-07-20 13:47:03 -0500

    renvio de correos cambios de codificación

--- application/modules/baw/views/responder_solicitud.php
+++ application/modules/baw/views/responder_solicitud.php
@@ -1,25 +1,25 @@
 <script>
 var postForm = function() {
 	var content = $('textarea[name="respuesta"]').html($('#summernote').code());
-} 
+}
 </script>
 <div class="page-header">
 	<h5><b>
     <img src="<?=base_url('assets/img/1399334070_user.png')?>"> <a class="bom-menu" href="<?=base_url('baw/home/index')?>">BIT&Aacute;CORA DE ATENCI&Oacute;N WEB / </a><a class="bom-menu" href="<?=base_url('baw/administrar/index')?>">ADMINISTRAR /</a>
 	<?php if($accion==0):?>
     <a class="bom-menu" href="<?=base_url('baw/administrar/atendiendose')?>">SOLICITUDES ATENDIENDOSE / </a>RESPUESTA
-    <?php else:?>
+	<?php elseif($action==1):?>
     <a class="bom-menu" href="<?=base_url('baw/administrar/registrados')?>">SOLICITUDES REGISTRADAS / </a>RESPUESTA
     <?php endif;?>
     </b></h5>
 </div>
 <?=form_open_multipart('baw/administrar/respuesta',array('class'=>'form-horizontal','role'=>'form','id'=>'respuesta'));?>
-<?php foreach($solicitudes as $solicitud):?>          
+<?php foreach($solicitudes as $solicitud):?>
 <div id="mensaje_respuesta">
     <div class="row">
         <div class="col-md-12">
             <div class="square">
-                <h5><strong>Por favor llene todos los campos</strong></h5>            
+                <h5><strong>Por favor llene todos los campos</strong></h5>
                 <br>
               <div class="form-group">
                 <label for="registro-plaza" class="col-sm-1 control-label">Folio: </label>
@@ -31,13 +31,17 @@ var postForm = function() {
                 <label for="respuesta" class="col-sm-1 control-label">Respuesta: </label>
                 <div class="col-sm-11">
                 	<textarea name="respuesta" id="summernote"></textarea>
-                    <!--<div class="required">Escribe el texto</div>-->               
+                    <!--<div class="required">Escribe el texto</div>-->
+									<?php
+										//Checar si venimos de una respuesta vacia, update
+										echo '<input type="hidden" name="accion" value="'. ($accion == 2 ? $accion : 0) .'"/>';
+									?>
                 </div>
               </div>
               <div class="form-group">
               	<label for="respuesta" class="col-sm-1 control-label">Archivos: </label>
                 <div class="col-sm-11">
-                  <input type="file" name="userfile[]" id="userfile" class="fileUpload" multiple>              
+                  <input type="file" name="userfile[]" id="userfile" class="fileUpload" multiple>
                 </div>
               </div>
             </div>
